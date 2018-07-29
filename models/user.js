71f54fb18c97acda60cad3c270fd503f00c9b2b4
Kazi Jawad <kazi_jawad@outlook.com>
2018-07-29 11:55:08 -0400

    Add password reset feature

--- models/user.js
+++ models/user.js
@@ -2,11 +2,21 @@ const mongoose = require('mongoose');
 const passportLocalMongoose = require('passport-local-mongoose');
 
 const userSchema = new mongoose.Schema({
-	username: String,
+	username: {
+		type: String,
+		unique: true,
+		required: true,
+	},
 	password: String,
 	firstName: String,
 	lastName: String,
-	email: String,
+	email: {
+		type: String,
+		unique: true,
+		required: true,
+	},
+	resetPasswordToken: String,
+	resetPasswordExpires: Date,
 	isAdmin: {
 		type: Boolean,
 		default: false,
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const passport = require('passport');
+const async = require('async');
+const nodemailer = require('nodemailer');
+const crypto = require('crypto');
 const User = require('../models/user');
 const Movie = require('../models/movie');
 const router = express.Router();
@@ -57,6 +60,119 @@ router.get('/logout', (req, res) => {
 	req.logout();
 	req.flash('success', 'Goodbye!');
 	res.redirect('/movies');
+});
+
+// PASSWORD RESET
+router.get('/reset', (req, res) => {
+	res.render('reset');
+});
+
+router.post('/reset', (req, res, next) => {
+	async.waterfall([
+		function(done) {
+			crypto.randomBytes(20, (error, buf) => {
+				const token = buf.toString('hex');
+				done(error, token);
+			});
+		},
+		function(token, done) {
+			User.findOne({ email: req.body.email }, (error, user) => {
+				if (!user) {
+					req.flash('error', 'No account with that email address exists.');
+					return res.redirect('/reset');
+				}
+
+				user.resetPasswordToken = token;
+				user.resetPasswordExpires = Date.now() + 3600000;
+
+				user.save(err => {
+					done(err, token, user);
+				});
+			});
+		},
+		function(token, user, done) {
+			const smtpTransport = nodemailer.createTransport({
+				service: 'Gmail',
+				auth: {
+					user: process.env.GMAIL,
+					pass: process.env.GMAIL_PW,
+				},
+			});
+			const mailOptions = {
+				to: user.email,
+				from: process.env.GMAIL,
+				subject: 'FilmTime Password Reset',
+				text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
+					'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
+					`http://${req.headers.host}/reset/${token}\n\n` +
+					'If you did not request this, please ignore this email and your password will remain unchanged.\n',
+			};
+			smtpTransport.sendMail(mailOptions, error => {
+				req.flash('success', `An email has been sent to ${user.email} with further instructions.`);
+				done(error, 'done');
+			});
+		},
+	], function(error) {
+		if (error) { return next(error); }
+		res.redirect('/reset');
+	});
+});
+
+// PASSWORD TOKEN
+router.get('/reset/:token', (req, res) => {
+	User.findOne({ resetPasswordToken: req.params.token, resetPasswordExpires: { $gt: Date.now() } }, (error, user) => {
+		if (!user) {
+			req.flash('error', 'Password token is invalid or has expired.');
+			return res.redirect('/reset');
+		}
+		res.render('new', { token: req.params.token });
+	});
+});
+
+router.post('/reset/:token', (req, res) => {
+	async.waterfall([
+		function(done) {
+			User.findOne({ resetPasswordToken: req.params.token, resetPasswordExpires: { $gt: Date.now() } }, (error, user) => {
+				if (!user) {
+					req.flash('error', 'Password reset token is invalid or has expired.');
+					return res.redirect('back');
+				} else if (req.body.password === req.body.confirm) {
+					user.setPassword(req.body.password, () => {
+						user.resetPasswordToken = undefined;
+						user.resetPasswordExpires = undefined;
+
+						user.save(function(err) {
+							done(err, user);
+						});
+					});
+				} else {
+					req.flash('error', 'Password does not match.');
+					return res.redirect('back');
+				}
+			});
+		},
+		function(user, done) {
+			const smtpTransport = nodemailer.createTransport({
+				service: 'Gmail',
+				auth: {
+					user: process.env.GMAIL,
+					pass: process.env.GMAIL_PW,
+				},
+			});
+			const mailOptions = {
+				to: user.email,
+				from: process.env.GMAIL,
+				subject: 'Your FilmTime password has been changed',
+				text: `Hello,\n\nThis is a confirmation that the password for your account, ${user.email}, has just been changed.\n`,
+			};
+			smtpTransport.sendMail(mailOptions, (error) => {
+				req.flash('success', 'Success! Your password has been changed.');
+				done(error);
+			});
+		},
+	], function() {
+		res.redirect('/movies');
+	});
 });
 
 // USER PROFILE
