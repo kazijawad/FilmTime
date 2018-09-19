04bafa23dafe85ccc51a597c607330abf1427619
Kazi Jawad <kazi_jawad@outlook.com>
2018-09-18 23:48:29 -0400

    Change flash messages

--- config/passport.config.js
+++ config/passport.config.js
@@ -17,7 +17,7 @@ passport.use(new LocalStrategy(
 		User.findOne({ username: username }, (error, user) => {
 			if (error) return done(error);
 			if (!user) {
-				return done(null, false, { message: 'Incorrect Username.' });
+				return done(null, false, { message: 'Incorrect Username' });
 			}
 			user.validPassword(password);
 
--- middleware/index.js
+++ middleware/index.js
@@ -5,26 +5,26 @@ module.exports = {
 	isLoggedIn: (req, res, next) => {
 		if (req.isAuthenticated()) { return next(); }
 
-		req.flash('error', 'You must be signed in to do that!');
+		req.flash('error', 'Please sign-in first');
 		res.redirect('/login');
 	},
 	isAdmin: (req, res, next) => {
 		if (req.user.isAdmin) { return next(); }
 
-		req.flash('error', 'You do not have permission to do that!');
+		req.flash('error', 'You do not have permission to do that');
 		res.redirect('back');
 	},
 	checkUserMovie: (req, res, next) => {
 		Movie.findById(req.params.id, (error, foundMovie) => {
 			if (error || !foundMovie) {
 				console.error(error);
-				req.flash('error', 'Sorry, that movie does not exist!');
+				req.flash('error', 'That movie does not exist');
 				res.redirect('/movies');
 			} else if (foundMovie.author.id.equals(req.user._id) || req.user.isAdmin) {
 				req.movie = foundMovie;
 				next();
 			} else {
-				req.flash('error', 'You do not have permission to do that!');
+				req.flash('error', 'You do not have permission to do that');
 				res.redirect('/movies/' + req.params.id);
 			}
 		});
@@ -33,13 +33,13 @@ module.exports = {
 		Comment.findById(req.params.commentId, (error, foundComment) => {
 			if (error || !foundComment) {
 				console.error(error);
-				req.flash('error', 'Sorry, that comment does not exist!');
+				req.flash('error', 'That comment does not exist');
 				res.redirect('/movies');
 			} else if (foundComment.author.id.equals(req.user._id) || req.user.isAdmin) {
 				req.comment = foundComment;
 				next();
 			} else {
-				req.flash('error', 'You do not have permission to do that!');
+				req.flash('error', 'You do not have permission to do that');
 				res.redirect('/movies/' + req.params.id);
 			}
 		});
