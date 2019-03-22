6a14bb845386e765a50c896d6e6b2583295f6b1d
Kazi Jawad <kazi_jawad@outlook.com>
2019-03-21 22:10:57 -0400

    Clean up movie routes

--- routes/movies.js
+++ routes/movies.js
@@ -4,31 +4,47 @@ const Comment = require('../models/comment');
 const { isLoggedIn, checkUserMovie } = require('../middleware/index');
 const router = express.Router();
 
+// HELPER FUNCTIONS
+function escapeRegex(text) {
+	return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
+}
+
+function compareMovies(a, b) {
+	if (a.title < b.title) {
+		return -1;
+	} else if (a.title > b.title) {
+		return 1;
+	}
+	return 0;
+}
+
 // INDEX
-router.get('/', (req, res) => {
+router.get('/', function(req, res) {
 	let noMatch = null;
+
 	if (req.query.search) {
 		const regex = new RegExp(escapeRegex(req.query.search), 'gi');
-		Movie.find({ title: regex }, (error, allMovies) => {
+
+		Movie.find({ title: regex }, function(error, allMovies) {
 			if (error) { return console.error(error); }
 			if (allMovies.length < 1) {
-				noMatch = 'No movies match that query, please try again.';
+				noMatch = 'No movies match that search, please try again.';
 			}
 
-			allMovies.sort(movieCompare);
+			allMovies.sort(compareMovies);
 			res.render('movies/index', { movies: allMovies, noMatch: noMatch, page: 'movies' });
 		});
 	} else {
-		Movie.find({}, (error, allMovies) => {
+		Movie.find({}, function(error, allMovies) {
 			if (error) { return console.error(error); }
-			allMovies.sort(movieCompare);
+			allMovies.sort(compareMovies);
 			res.render('movies/index', { movies: allMovies, noMatch: noMatch, page: 'movies' });
 		});
 	}
 });
 
 // CREATE
-router.post('/', isLoggedIn, (req, res) => {
+router.post('/', isLoggedIn, function(req, res) {
 	const title = req.body.title;
 	const description = req.body.description;
 	const poster = req.file ? { data: req.file.buffer, mimeType: req.file.mimetype } : null;
@@ -38,20 +54,20 @@ router.post('/', isLoggedIn, (req, res) => {
 	};
 
 	const newMovie = { title: title, description: description, poster: poster, author: author };
-	Movie.create(newMovie, (error) => {
+	Movie.create(newMovie, function(error) {
 		if (error) { return console.error(error); }
 		res.redirect('/movies');
 	});
 });
 
 // NEW
-router.get('/new', isLoggedIn, (req, res) => {
+router.get('/new', isLoggedIn, function(req, res) {
 	res.render('movies/new');
 });
 
 // SHOW
-router.get('/:id', (req, res) => {
-	Movie.findById(req.params.id).populate('comments').exec((error, foundMovie) => {
+router.get('/:id', function(req, res) {
+	Movie.findById(req.params.id).populate('comments').exec(function(error, foundMovie) {
 		if (error || !foundMovie) {
 			console.error(error);
 			req.flash('error', 'That movie does not exist.');
@@ -62,18 +78,18 @@ router.get('/:id', (req, res) => {
 });
 
 // EDIT
-router.get('/:id/edit', isLoggedIn, checkUserMovie, (req, res) => {
+router.get('/:id/edit', isLoggedIn, checkUserMovie, function(req, res) {
 	res.render('movies/edit', { movie: req.movie });
 });
 
 // UPDATE
-router.put('/:id', (req, res) => {
+router.put('/:id', function(req, res) {
 	const title = req.body.title;
 	const description = req.body.description;
 	const poster = req.file ? { data: req.file.buffer, mimeType: req.file.mimetype } : null;
 	const newData = poster ? { title: title, description: description, poster: poster } : { title: title, description: description };
 
-	Movie.findByIdAndUpdate(req.params.id, { $set: newData }, (error, movie) => {
+	Movie.findByIdAndUpdate(req.params.id, { $set: newData }, function(error, movie) {
 		if (error) {
 			req.flash('error', error.message);
 			return res.redirect('back');
@@ -83,39 +99,26 @@ router.put('/:id', (req, res) => {
 });
 
 // DELETE
-router.delete('/:id', isLoggedIn, checkUserMovie, (req, res) => {
+router.delete('/:id', isLoggedIn, checkUserMovie, function(req, res) {
 	Comment.remove({
 		__id: {
 			$in: req.movie.comments,
 		},
-	}, error => {
+	}, function(error) {
 		if (error) {
 			req.flash('error', error.message);
 			return res.redirect('/');
 		}
 
-		req.movie.remove(err => {
-			if (err) {
-				req.flash('error', err.message);
+		req.movie.remove(function(error) {
+			if (error) {
+				req.flash('error', error.message);
 				return res.redirect('/');
 			}
-			req.flash('error', 'Movie Deleted');
+			req.flash('error', 'Movie has been deleted.');
 			res.redirect('/movies');
 		});
 	});
 });
-
-const escapeRegex = text => {
-	return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
-};
-
-const movieCompare = (a, b) => {
-	if (a.title < b.title) {
-		return -1;
-	} else if (a.title > b.title) {
-		return 1;
-	}
-	return 0;
-};
 
 module.exports = router;
