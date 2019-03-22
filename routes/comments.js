9fc8cd9854888ad6822ea95570f1bdab8730567c
Kazi Jawad <kazi_jawad@outlook.com>
2019-03-21 22:19:00 -0400

    Clean up comments route

--- routes/comments.js
+++ routes/comments.js
@@ -5,43 +5,43 @@ const { isLoggedIn, isAdmin, checkUserComment } = require('../middleware/index')
 const router = express.Router({ mergeParams: true });
 
 // NEW
-router.get('/new', isLoggedIn, (req, res) => {
-	Movie.findById(req.params.id, (error, movie) => {
+router.get('/new', isLoggedIn, function(req, res) {
+	Movie.findById(req.params.id, function(error, movie) {
 		if (error) { return console.error(error); }
 		res.render('comments/new', { movie: movie });
 	});
 });
 
 // CREATE
-router.post('/', isLoggedIn, (req, res) => {
-	Movie.findById(req.params.id, (error, movie) => {
+router.post('/', isLoggedIn, function(req, res) {
+	Movie.findById(req.params.id, function(error, movie) {
 		if (error) {
 			console.error(error);
 			return res.redirect('/movies');
 		}
 
-		Comment.create(req.body.comment, (err, comment) => {
-			if (err) { return console.error(err); }
+		Comment.create(req.body.comment, function(error, comment) {
+			if (error) { return console.erroror(error); }
 
 			comment.author.id = req.user._id;
 			comment.author.username = req.user.username;
 			comment.save();
 			movie.comments.push(comment);
 			movie.save();
-			req.flash('success', 'Comment created');
+			req.flash('success', `Comment added to ${movie.title}.`);
 			res.redirect(`/movies/${movie._id}`);
 		});
 	});
 });
 
 // EDIT
-router.get('/:commentId/edit', isLoggedIn, checkUserComment, (req, res) => {
+router.get('/:commentId/edit', isLoggedIn, checkUserComment, function(req, res) {
 	res.render('comments/edit', { movie_id: req.params.id, comment: req.comment });
 });
 
 // UPDATE
-router.put('/:commentId', isAdmin, (req, res) => {
-	Comment.findByIdAndUpdate(req.params.commentId, req.body.comment, error => {
+router.put('/:commentId', isAdmin, function(req, res) {
+	Comment.findByIdAndUpdate(req.params.commentId, req.body.comment, function(error) {
 		if (error) {
 			console.error(error);
 			return res.render('edit');
@@ -50,29 +50,28 @@ router.put('/:commentId', isAdmin, (req, res) => {
 	});
 });
 
-// DESTROY
-router.delete('/:commentId', isLoggedIn, checkUserComment, (req, res) => {
+// DELETE
+router.delete('/:commentId', isLoggedIn, checkUserComment, function(req, res) {
 	Movie.findByIdAndUpdate(req.params.id, {
 		$pull: {
 			comments: req.comment.id,
 		},
-	}, error => {
+	}, function(error) {
 		if (error) {
 			console.error(error);
 			req.flash('error', error.message);
 			return res.redirect('/');
 		}
 
-		req.comment.remove(err => {
-			if (err) {
-				req.flash('error', err.message);
+		req.comment.remove(function(error) {
+			if (error) {
+				req.flash('error', error.message);
 				return res.redirect('/');
 			}
-			req.flash('error', 'Comment deleted!');
+			req.flash('error', 'Comment deleted.');
 			res.redirect(`/movies/${req.params.id}`);
 		});
-	}
-	);
+	});
 });
 
 module.exports = router;
